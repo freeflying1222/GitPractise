fb3e288360b40afbb3af769a7c7768f26d676717
yz-yefei <2282636022@qq.com>
2020-06-01 10:04:00 +0800

    //from pk

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace GitPractise
         {
             ///当前：release，修复bug
             Console.WriteLine("Hello World!");//master：avoid conflict
+            //from pk
             ///当前 master， 提交-1
             ///当前 master， 继续工作，提交-1
             ///当前 fg，提交-1
