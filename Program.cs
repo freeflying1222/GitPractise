22df9a0f3d39f4496096ea7e98d4b7143918555c
yz-yefei <2282636022@qq.com>
2020-06-01 09:35:41 +0800

    //master：avoid conflict

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace GitPractise
         static void Main(string[] args)
         {
             ///当前：release，修复bug
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//master：avoid conflict
             ///当前 master， 提交-1
             ///当前 master， 继续工作，提交-1
             ///
